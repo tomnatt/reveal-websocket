a8138aa1ab4dd1e73175823f8e76b2f9b20877af
Tom <github@tomnatt.com>
2012-05-25 17:55:55 +0100

    different investigations

--- app/controllers/MyWebSocket.java
+++ app/controllers/MyWebSocket.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.libs.F.*;
 import play.mvc.*;
 import play.mvc.Http.*;
 
@@ -9,33 +10,48 @@ import java.util.*;
 import models.*;
 
 public class MyWebSocket extends WebSocketController {
-
+    
     public static void hello(String name) {
         System.out.println("Hello " + name);
         outbound.send("Hello %s!", name);
     }
     
     public static void echo() {
+        
         while (inbound.isOpen()) {
-            WebSocketEvent e = await(inbound.nextEvent());
+            //WebSocketEvent e = await(inbound.nextEvent());
+            //WebSocketEvent e = await(StatefulModel.instance.event.nextEvent());
+            
+            
+            //StatefulModel.instance.event.publish
+            
+            Object event = await(StatefulModel.instance.event.nextEvent());
+            System.out.println(event.getClass().toString());
+            //outbound.send(event);
+            
+            /*
             if (e instanceof WebSocketFrame) {
+                
                 WebSocketFrame frame = (WebSocketFrame)e;
                 if (!frame.isBinary) {
                     if(frame.textData.equals("quit")) {
-                    	System.out.println("quit");
                         outbound.send("Bye!");
                         disconnect();
                     } else {
-                    	System.out.println("not quit");
+                        
+                        System.out.println(frame.textData);
                         outbound.send("Echo: %s", frame.textData);
+                        
+                        
                     }
                 }
             }
+            
             if (e instanceof WebSocketClose) {
-                System.out.println("closing");
                 Logger.info("Socket closed!");
             }
-            System.out.println("howdy");
+            */
+            
         }
     }
     
